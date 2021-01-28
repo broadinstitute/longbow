de7348cbd9496c85728f1d1453e7a9a90f973df1
Jonn Smith <jonn@broadinstitute.org>
2021-01-28 15:12:01 -0500

    Added threading and bam output.

--- src/annmas/segment/command.py
+++ src/annmas/segment/command.py
@@ -5,7 +5,22 @@ import click_log
 import multiprocessing as mp
 from multiprocessing import Process, Manager
 
+import concurrent.futures
+
+from collections import namedtuple
+
 from ..utils.model import *
+
+# Named tuple to store alignment information:
+class SegmentInfo(namedtuple(
+    "SegmentInfo", ["name", "start", "end"]
+)):
+    def __str__(self):
+        return f"SegmentInfo({self.to_tag()})"
+
+    def to_tag(self):
+        return f"{self.name}:{self.start}-{self.end}"
+
 
 logger = logging.getLogger(__name__)
 click_log.basic_config(logger)
@@ -22,22 +37,64 @@ def main(model, threads, output_bam, input_bam):
     logger.info(f"annmas: segment started")
 
     threads = mp.cpu_count() if threads <= 0 or threads > mp.cpu_count() else threads
-    logger.info(f"running with {threads} thread(s)")
+    logger.info(f"Running with {threads} thread(s)")
 
     m = build_default_model()
     if model is not None:
         m.from_yaml(model)
-        logger.info(f"using pretrained annotation model {model}")
+        logger.info(f"Using pretrained annotation model {model}")
     else:
-        logger.info(f"using default annotation model")
+        logger.info(f"Using default annotation model")
+
+    num_reads_annotated = 0
+    num_sections = 0
 
     pysam.set_verbosity(0)  # silence message about the .bai file not being found
-    with pysam.AlignmentFile(input_bam, "rb", check_sq=False, require_index=False) as bf:
-        for read in bf:
-            print(read.query_name)
-            segment_read(read, m)
+    with pysam.AlignmentFile(input_bam, "rb", check_sq=False, require_index=False) as bam_file:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+            future_to_segmented_read = {executor.submit(segment_read, r, m): r for r in bam_file}
+
+            out_bam_header_dict = {'HD': {'VN': '1.0', 'SO': "unknown", 'pb': '>=3.01'}}
+            with pysam.AlignmentFile(output_bam, 'wb', header=out_bam_header_dict) as out_bam_file:
+
+                for future in concurrent.futures.as_completed(future_to_segmented_read):
+                    read = future_to_segmented_read[future]
+                    try:
+                        path, logp = future.result()
+                    except Exception as ex:
+                        logger.error('%r generated an exception: %s', read, ex)
+                    else:
+                        # Condense the output annotations so we can write them out with indices:
+                        segments = collapse_annotations(path)
+
+                        # Obligatory log message:
+                        logger.debug("Path for read %s (%2.2f): %s", read.query_name, logp, segments)
+
+                        # Set our tag and write out the read:
+                        read.set_tag("SG", "|".join([s.to_tag() for s in segments]))
+                        out_bam_file.write(read)
+
+                        # Increment our counters:
+                        num_reads_annotated += 1
+                        num_sections += len(segments)
 
     logger.info("annmas: segment finished")
+    logger.info(f"annmas: segmented {num_reads_annotated} reads into {num_sections} sections")
+
+
+def collapse_annotations(path):
+    last = ""
+    start = 0
+    segments = []
+    for i, seg in enumerate(path):
+        if seg != last:
+            if i != 0:
+                segments.append(SegmentInfo(last, start, i - 1))
+            last = seg
+            start = i
+    # Don't forget the last one:
+    segments.append(SegmentInfo(last, start, i - 1))
+    return segments
 
 
 def segment_read(read, um):
@@ -48,3 +105,5 @@ def segment_read(read, um):
         if logp > flogp:
             flogp = logp
             fppath = ppath
+
+    return fppath, flogp
