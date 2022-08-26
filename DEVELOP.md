b72aa73e3dccd4c4acec35fded1bd0880aa56198
BumpVersion Action <bumpversion@github-actions>
2022-08-26 15:19:43 +0000

    Bump version: 0.5.37 → 0.5.38

--- DEVELOP.md
+++ DEVELOP.md
@@ -1,7 +1,7 @@
 # Longbow 
 Annotation and segmentation of MAS-seq data
 
-Current version: 0.5.37
+Current version: 0.5.38
 
 ## Development
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 # following src dir layout according to
 # https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
-version = "0.5.37"
+version = "0.5.38"
 setup(
     name="maslongbow",
     version=version,
