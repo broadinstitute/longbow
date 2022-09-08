20f828f2c99e0028046dfa9043d42133070ab41a
Jonn Smith <jonn@broadinstitute.org>
2022-09-08 15:45:42 -0400

    Added hard progress logging in `sift`.

--- src/longbow/sift/command.py
+++ src/longbow/sift/command.py
@@ -105,11 +105,11 @@ def main(pbi, output_bam, reject_bam, model, validation_model, force, stats, sum
     read_count = None
     if os.path.exists(pbi):
         read_count = bam_utils.load_read_count(pbi)
-        logger.info("About to Filter %d reads", read_count)
+        logger.info("About to Sift %d reads", read_count)
     else:
         read_count = bam_utils.get_read_count_from_bam_index(input_bam)
         if read_count:
-            logger.info("About to Filter %d reads", read_count)
+            logger.info("About to Sift %d reads", read_count)
 
     reads_to_ignore = set()
     if ignore_list and os.path.exists(ignore_list):
@@ -154,6 +154,12 @@ def main(pbi, output_bam, reject_bam, model, validation_model, force, stats, sum
         # Get our header from the input bam file:
         out_header = bam_utils.create_bam_header_with_program_group(logger.name, bam_file.header, models=[lb_model])
 
+        # Define a hard print interval that will periodically let the user know something is going on:
+        if read_count:
+            hard_print_interval = int(read_count * 0.1)
+        else:
+            hard_print_interval = 100000
+
         # Setup output files:
         with pysam.AlignmentFile(output_bam, "wb", header=out_header) as passing_bam_file, \
                 pysam.AlignmentFile(reject_bam, "wb", header=out_header) as failing_bam_file, \
@@ -169,7 +175,7 @@ def main(pbi, output_bam, reject_bam, model, validation_model, force, stats, sum
 
             stats_file.write('\t'.join(['read_name', 'rq', '5p_Adapter', 'CBC', 'UMI', 'SLS', 'cDNA', 'Poly_A', '3p_Adapter', 'SG']) + '\n')
 
-            for read in tqdm.tqdm(bam_file, desc="Progress", unit=" read", colour="green", file=sys.stderr, disable=not sys.stdin.isatty(), total=read_count):
+            for i, read in enumerate(tqdm.tqdm(bam_file, desc="Progress", unit=" read", colour="green", file=sys.stderr, disable=not sys.stdin.isatty(), total=read_count)):
 
                 if read.query_name in reads_to_ignore:
                     logger.debug(f"Ignoring read: {read.query_name}")
@@ -233,6 +239,12 @@ def main(pbi, output_bam, reject_bam, model, validation_model, force, stats, sum
                         adapter_pattern
                         ]) + '\n'
                     )
+
+                if (i % hard_print_interval) == 0:
+                    if read_count:
+                        logger.info("Sifted %d/%d (%2.2f%%) reads.", i, read_count, 100*(i/read_count))
+                    else:
+                        logger.info("Sifted %d reads.", i)
 
     # Calc some stats:
     tot_reads = num_passed + num_failed + num_ignored
