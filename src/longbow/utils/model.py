e53cb7540279fa30539a8e2c8edc1948d847ca83
Kiran V Garimella <kiran@broadinstitute.org>
2022-10-03 00:48:37 -0400

    Populate 'array_element_structure' from hierarchical model in a manner compatible with the segment command

--- src/longbow/utils/model.py
+++ src/longbow/utils/model.py
@@ -45,6 +45,39 @@ class LibraryModel:
         self.key_adapters = self.array_model['structure']
         self.key_adapter_set = set(self.key_adapters)
 
+        # self.array_element_structure = self.array_model['structure']
+        # "array_element_structure": (
+        #         ("A", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("B", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("C", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("D", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("E", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("F", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("G", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("H", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("I", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("J", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("K", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("L", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("M", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("N", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind"),
+        #         ("O", "TPV2_adapter", "cDNA", "Poly_A", "idx", "rev_bind", "P"),
+        #     ),
+
+        l1 = []
+        for i in range(len(self.array_model['structure']) - 1):
+            ae = self.array_model['structure'][i]
+            l1.append([ae] + self.cdna_model['structure'])
+
+        ae = self.array_model['structure'][-1]
+        l1[-1].append(ae)
+
+        l2 = []
+        for l in l1:
+            l2.append(tuple(l))
+
+        self.array_element_structure = tuple(l2)
+
         if do_build:
             self.build()
 
