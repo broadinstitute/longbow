6669992dbae33d92a6daec1f406de749dc521a37
Kiran V Garimella <kiran@broadinstitute.org>
2022-11-06 01:03:44 -0500

    Add a pseudocount for num_reads to prevent a rare divide-by-zero error

--- src/longbow/commands/segment.py
+++ src/longbow/commands/segment.py
@@ -194,7 +194,7 @@ def main(threads, output_bam, create_barcode_conf_file, model, ignore_cbc_and_um
     logger.info(
         f"Segmented {res['num_reads_segmented']} reads with {res['num_segments']} total segments."
     )
-    num_reads = res['num_reads_segmented']
+    num_reads = res['num_reads_segmented']+1
     num_segmented = res['num_segments']
     logger.info(f"MAS-seq gain factor: {num_segmented/num_reads:.02f}x")
     logger.info(f"Done. Elapsed time: %2.2fs.", time.time() - t_start)
